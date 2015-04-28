822e77df280bb3e612368eb74ae69491b342b990
mbojey <mbojey@gmail.com>
2015-04-27 17:52:58 -0700

    Added tutorial video

--- getMarks.rb
+++ getMarks.rb
@@ -1,20 +1,15 @@
-@users = User.all.uniq(:studentnumber)
+@users = Scene.where.not(submitted_at: nil)
+p ("" "Creator id" + " , " + "Collab id" + " , "+ "Created at" + " , "+ "Submitted at" + " , "+ "Question" + " , "+ "Correct" + " , "+ "Hint" + " , " + "Time Spent")
 
-@userary = [];
-@marksary = [];
-p ("" + "Student Number" + ", " + "Score" + "\n")
 @users.each do |u|
-	@scenes = Scene.where("creator_id = ? or collab_id = ?", u.id, u.id)
-	@score = ((2*@scenes.where(correct: true, hint: true).uniq.pluck(:question).count + 
-			 4*@scenes.where(correct: true, hint: nil, collab_id: 0).uniq.pluck(:question).count + 
-			 5*(@scenes.where(correct: true, hint: nil).uniq.pluck(:question).count - 
-			 @scenes.where(correct: true, hint: nil, collab_id: 0).uniq.pluck(:question).count))/80.0)*30
-	@userary.push(u.studentnumber)
-	@marksary.push(@score)
-	begin
-  		p ("" + u.studentnumber.to_s + ", " + @score.to_s + "\n") 
-	rescue IOError => e
-  	#some error occur, dir not writable etc.
-	end
+	responses = "INSERT INTO Scenes VALUES("
+	responses += "" + u.creator_id.to_s + ","
+	responses += "" + u.collab_id.to_s + ","
+	responses += "" + u.created_at.to_i.to_s+ ","
+	responses += "" + u.submitted_at.to_i.to_s + ","
+	responses += "" + u.question.to_s + ","
+	responses += "" + u.correct.to_s + ","
+	responses += "" + u.hint.to_s + ","
+	responses += "" + (DateTime.parse(u.submitted_at.to_s).to_time - DateTime.parse(u.created_at.to_s).to_time).to_s + ");"
+	p responses
 end
-file.close unless file == nil
